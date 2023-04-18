6829d9ee2995ff23bd44490c37721ac8ab196685
Paulo <paulofilhobarroso@gmail.com>
2023-04-17 22:02:53 -0300

    feat: Extracting functions of js_to_python function.

--- main.py
+++ main.py
@@ -1,25 +1,56 @@
 def js_to_python(js_code_to_translate):
-    js_code_to_translate = js_code_to_translate.replace('console.log', 'print')
-    js_code_to_translate = js_code_to_translate.replace('var', '')
-    js_code_to_translate = js_code_to_translate.replace('let', '')
-    js_code_to_translate = js_code_to_translate.replace('const', '')
-    js_code_to_translate = js_code_to_translate.replace('===', '==')
-    js_code_to_translate = js_code_to_translate.replace('!==', '!=')
-    js_code_to_translate = js_code_to_translate.replace('++', '+= 1')
-    js_code_to_translate = js_code_to_translate.replace('--', '-= 1')
-    js_code_to_translate = js_code_to_translate.replace('+=', '+= ')
-    js_code_to_translate = js_code_to_translate.replace('-=', '-= ')
-    js_code_to_translate = js_code_to_translate.replace('*=', '*= ')
-    js_code_to_translate = js_code_to_translate.replace('/=', '/= ')
-    js_code_to_translate = js_code_to_translate.replace('%=', '%= ')
-    js_code_to_translate = js_code_to_translate.replace(';', '\n')
+    remove_reserved_words(js_code_to_translate)
+    change_comparison_operators(js_code_to_translate)
+    change_incremental_operators(js_code_to_translate)
 
-    js_code_to_translate = 'def ' + js_code_to_translate.split('(', 1)[0].split()[-1] + '(' + js_code_to_translate.split('(', 1)[1]
+    js_code_to_translate = 'def ' + js_code_to_translate.split('(', 1)[0].split()[-1] + '(' + \
+                           js_code_to_translate.split('(', 1)[1]
     js_code_to_translate = js_code_to_translate.replace('}', '')
     js_code_to_translate = js_code_to_translate.replace('{', ':')
     js_code_to_translate = js_code_to_translate.replace('else if', 'elif')
 
     return js_code_to_translate
+
+
+def remove_reserved_words(code_to_translate):
+    variable_reserved_words = ['var', 'let', 'const']
+
+    reserved_words = {
+        'console.log': 'print',
+        ';': '\n'
+    }
+
+    for word in variable_reserved_words:
+        code_to_translate.replace(word, '')
+
+    for operator in reserved_words.keys():
+        code_to_translate.replace(operator, reserved_words[operator])
+    return code_to_translate
+
+
+def change_comparison_operators(code_to_translate):
+    comparison_operators = {
+        '===': '==',
+        '!==': '!='
+    }
+    for operator in comparison_operators.keys():
+        code_to_translate.replace(operator, comparison_operators[operator])
+    return code_to_translate
+
+
+def change_incremental_operators(code_to_translate):
+    comparison_operators = {
+        '++': '+= 1',
+        '--': '-= 1',
+        '*=': '*= ',
+        '/=': '/= ',
+        '%=': '%= ',
+        '+=': '+= ',
+        '-=': '-= ',
+    }
+    for operator in comparison_operators.keys():
+        code_to_translate.replace(operator, comparison_operators[operator])
+    return code_to_translate
 
 
 example1 = '''
